a285b55317190a9a2bdfdd31212714cefcc0ba6a
SACRED GAMES 3.05 <106374961+YounisJohn@users.noreply.github.com>
2022-07-10 14:21:19 +0500

    Update X5.py

--- X5.py
+++ X5.py
@@ -1,5 +1,5 @@
 from Publicv5 import login
-
-
+print("\n\x1b[1;92m Congratulations Beb Your Device Support This Tool\033[1;37m")
+print("          \033[1;91mUse Vpn If Tool Run Error!") 
 
 login()
