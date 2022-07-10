1abad8b5c03ee48e492fa13ff5f44b8edd8f391f
SACRED GAMES 3.05 <106374961+YounisJohn@users.noreply.github.com>
2022-07-10 14:24:23 +0500

    Update X5.py

--- X5.py
+++ X5.py
@@ -22,7 +22,7 @@ if bit == '64bit':
 
     print("\n\x1b[1;92m Congratulations Beb Your Device Support This Tool\033[1;37m")
 
-   print("          \033[1;91mUse Vpn If Tool Run Error!") 
+    print("          \033[1;91mUse Vpn If Tool Run Error!") 
 
     from Publicv5 import login
 
