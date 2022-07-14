f0facfa5275e4783fe7e7266a91b913fd90c37b6
SACRED GAMES 3.05 <106374961+YounisJohn@users.noreply.github.com>
2022-07-14 12:34:55 +0500

    Update X5.py

--- X5.py
+++ X5.py
@@ -2,9 +2,9 @@ import os
 
 os.system('clear')
 
-x = '\t>> Script has been permanent deleted and john owner can not more update it'
+x = '>> Script has been permanent deleted and john owner can not more update it'
 
-y = '\t>> Thanks for using Younis John tolls'
+y = '>> Thanks for using Younis John tolls'
 
 print(x)
 
